101879a773d64a91807f09a733ed6e0817f107f0
chirag miracle <158419139+chiragmiracle@users.noreply.github.com>
2024-04-02 11:03:03 +0530

    Create Project

---
+++ app/src/main/java/com/miracle/miraclekit/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.miracle.miraclekit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorAccent = Color(0xFF007F73)
+val ColorPrimary = Color(0xFF007F73)
+val ColorPrimaryDark = Color(0xFF007F73)
+val Theme_Clr = Color(0xFF007F73)
+val Light_Clr = Color(0xFF00C2AF)
+val BgColor = Color(0xFFFFFFFF)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
